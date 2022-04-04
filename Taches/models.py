376bd47b18485d2171ce4cee793f381358fc1c66
hechem <hechembennasr22@gmail.com>
2022-04-05 00:54:58 +0100

    commit

--- Taches/models.py
+++ Taches/models.py
@@ -19,7 +19,7 @@ class Tache(models.Model):
 
 class Affectation(models.Model):
    Tache =models.ForeignKey( Tache , null=True, on_delete=models.SET_NULL)
-   User = models.ForeignKey( User , null=True, on_delete=models.SET_NULL)
+   User = models.ForeignKey( User , null=True, on_delete=models.SET_NULL )
 
 
    
